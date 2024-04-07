363798c379256f16f74b2e5a7c3c7c49e540bed0
Dino Source <dinosource.cpp@gmail.com>
2024-04-07 16:20:14 -0700

    Simplify code + add helper scripts to build & run main app and tests

--- src/solution.h
+++ src/solution.h
@@ -35,26 +35,9 @@ public:
         }
     }
 
-    // This second overloaded version of quick_sort is optional.
-    // The purpose of this version is to be able calling quick_sort
-    // function with one argument only (a collection of elements).
     template<typename CollectionType>
     static void quick_sort(CollectionType &coll) {
-        if (coll.begin() < coll.end()) {
-            auto pivot {std::next(coll.end(), -1)};
-            auto i {std::next(coll.begin(), -1)};
-
-            for (auto j {coll.begin()}; j != coll.end(); ++j) {
-                if (*j < *pivot) {
-                    std::iter_swap(++i, j);
-                }
-            }
-            std::iter_swap(++i, pivot);
-
-            pivot = i;
-            quick_sort(coll.begin(), pivot);
-            quick_sort(std::next(pivot, 1), coll.end());
-        }
+        quick_sort(coll.begin(), coll.end());
     }
 };
 
